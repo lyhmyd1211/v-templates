001e83143e2942d63438d71a25ae01e7777f9ed8
luyingheng <75986899@qq.com>
2020-06-05 14:30:16 +0800

    update permission

--- vue-admin-template/src/permission.js
+++ vue-admin-template/src/permission.js
@@ -19,77 +19,76 @@ router.beforeEach(async(to, from, next) => {
   document.title = getPageTitle(to.meta.title)
   // determine whether the user has logged in
   const hasToken = getToken()
-  next()
-  // if (hasToken) {
-  //   if (to.path === '/login') {
-  //     // if is logged in, redirect to the home page
-  //     next({ path: '/' })
-  //     NProgress.done()
-  //   } else {
-  //     // determine whether the user has obtained his permission roles through getInfo
-  //     const hasRoles = store.getters.roles && store.getters.roles.length > 0
-  //     if (hasRoles) {
-  //       if (to.path === '/') {
-  //         const accessRoutes = await store.dispatch('permission/generateRoutes', store.getters.roles)
-  //         const path = await getFirstRouterByMap(accessRoutes)
-  //         next(path)
-  //       } else {
-  //         if (BlackList.indexOf(to.path) !== -1) {
-  //           if (!store.getters.roles.includes(-1)) { // blackList powers now is -1
-  //             MessageBox.alert('当前用户没有权限无法进入', '提示', {
-  //               confirmButtonText: '确定',
-  //               type: 'warning',
-  //               callback: action => {
-  //                 NProgress.done()
-  //               }
-  //             })
-  //           } else {
-  //             next()
-  //           }
-  //         } else {
-  //           next()
-  //         }
-  //       }
-  //     } else {
-  //       try {
-  //         // get user info
-  //         // note: roles must be a object array! such as: ['admin'] or ,['developer','editor']
-  //         await store.dispatch('user/getInfo')
-  //         // generate accessible routes map based on roles
-  //         const accessRoutes = await store.dispatch('permission/generateRoutes', store.getters.roles)
-  //         // dynamically add accessible routes
-  //         await router.addRoutes(accessRoutes)
-  //         // await store.dispatch('notification/GetNotify', store.getters.userinfo)
-  //         if (from.path === '/login') {
-  //           const path = await getFirstRouterByMap(accessRoutes)
-  //           await next(path)
-  //         } else {
-  //           await next(to.path)
-  //         }
-  //         // hack method to ensure that addRoutes is complete
-  //         // set the replace: true, so the navigation will not leave a history record
-  //       } catch (error) {
-  //         console.log('eeeeeeeeeeeeeeeeeeeee', error)
-  //         // remove token and go to login page to re-login
-  //         await store.dispatch('user/resetToken')
-  //         Message.error(error || 'Has Error')
-  //         next(`/login?redirect=${to.path}`)
-  //         NProgress.done()
-  //       }
-  //     }
-  //   }
-  // } else {
-  //   /* has no token*/
+  if (hasToken) {
+    if (to.path === '/login') {
+      // if is logged in, redirect to the home page
+      next({ path: '/' })
+      NProgress.done()
+    } else {
+      // determine whether the user has obtained his permission roles through getInfo
+      const hasRoles = store.getters.roles && store.getters.roles.length > 0
+      if (hasRoles) {
+        if (to.path === '/') {
+          const accessRoutes = await store.dispatch('permission/generateRoutes', store.getters.roles)
+          const path = await getFirstRouterByMap(accessRoutes)
+          next(path)
+        } else {
+          if (BlackList.indexOf(to.path) !== -1) {
+            if (!store.getters.roles.includes(-1)) { // blackList powers now is -1
+              MessageBox.alert('当前用户没有权限无法进入', '提示', {
+                confirmButtonText: '确定',
+                type: 'warning',
+                callback: action => {
+                  NProgress.done()
+                }
+              })
+            } else {
+              next()
+            }
+          } else {
+            next()
+          }
+        }
+      } else {
+        try {
+          // get user info
+          // note: roles must be a object array! such as: ['admin'] or ,['developer','editor']
+          await store.dispatch('user/getInfo')
+          // generate accessible routes map based on roles
+          const accessRoutes = await store.dispatch('permission/generateRoutes', store.getters.roles)
+          // dynamically add accessible routes
+          await router.addRoutes(accessRoutes)
+          // await store.dispatch('notification/GetNotify', store.getters.userinfo)
+          if (from.path === '/login') {
+            const path = await getFirstRouterByMap(accessRoutes)
+            await next(path)
+          } else {
+            await next(to.path)
+          }
+          // hack method to ensure that addRoutes is complete
+          // set the replace: true, so the navigation will not leave a history record
+        } catch (error) {
+          console.log('eeeeeeeeeeeeeeeeeeeee', error)
+          // remove token and go to login page to re-login
+          await store.dispatch('user/resetToken')
+          Message.error(error || 'Has Error')
+          next(`/login?redirect=${to.path}`)
+          NProgress.done()
+        }
+      }
+    }
+  } else {
+    /* has no token*/
 
-  //   if (whiteList.indexOf(to.path) !== -1) {
-  //     // in the free login whitelist, go directly
-  //     next()
-  //   } else {
-  //     // other pages that do not have permission to access are redirected to the login page.
-  //     next(`/login?redirect=${to.path}`)
-  //     NProgress.done()
-  //   }
-  // }
+    if (whiteList.indexOf(to.path) !== -1) {
+      // in the free login whitelist, go directly
+      next()
+    } else {
+      // other pages that do not have permission to access are redirected to the login page.
+      next(`/login?redirect=${to.path}`)
+      NProgress.done()
+    }
+  }
 })
 
 router.afterEach(() => {
--- vue-admin-template/src/views/login/index.vue
+++ vue-admin-template/src/views/login/index.vue
@@ -46,13 +46,16 @@
           <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
         </span>
       </el-form-item>
-
       <el-button
         :loading="loading"
         type="primary"
         style="width:100%;margin-bottom:30px;"
         @click.native.prevent="handleLogin"
       >登录</el-button>
+      <div class="tips">
+        <span style="margin-right:20px;">username: admin</span>
+        <span>password: any</span>
+      </div>
     </el-form>
   </div>
 </template>
