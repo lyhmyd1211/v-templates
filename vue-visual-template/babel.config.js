c38ea1cf96acb8ce6d2076dd7dd5fc483b76e4aa
luyingheng <75986899@qq.com>
2020-06-15 17:34:50 +0800

    save only admin  template in this branch

--- vue-visual-template/babel.config.js
+++
@@ -1,8 +0,0 @@
-const plugins = []
-module.exports = {
-  presets: [['@vue/app', { 'useBuiltIns': 'entry' }], ['@babel/preset-env', {
-    'useBuiltIns': 'usage',
-    'corejs': 3
-  }]],
-  plugins: plugins
-}
