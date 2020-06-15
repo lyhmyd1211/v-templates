0252d506cd162a67f4064b977841d600e877eaf0
luyingheng <75986899@qq.com>
2020-06-15 17:26:49 +0800

    save only visual template in this branch

--- vue-app-template/src/plugins/vant.js
+++
@@ -1,5 +0,0 @@
-import { Cell, CellGroup, Icon, Tag, Swipe, SwipeItem, SubmitBar, Checkbox, Notify, Toast } from 'vant';
-
-export   const  vant =  {
-  Cell, CellGroup, Icon, Tag, Swipe, SwipeItem, SubmitBar, Checkbox,Notify,Toast
-}
