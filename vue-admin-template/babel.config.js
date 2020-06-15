502f51aacbf6f7f12fb1b22222dcea579ce66643
luyingheng <75986899@qq.com>
2020-06-15 17:37:11 +0800

    save only app template in this branch

--- vue-admin-template/babel.config.js
+++
@@ -1,5 +0,0 @@
-const plugins = []
-module.exports = {
-  presets: [['@vue/app', { 'useBuiltIns': 'entry' }]],
-  plugins: plugins
-}
--- vue-visual-template/src/api/dataFetch.js
+++
@@ -1,8 +0,0 @@
-import request from '@/utils/request'
-export function getWeather(params) {
-	return request({
-		url: 'https://free-api.heweather.net/s6/weather/forecast',
-		method: 'get',
-		params
-	})
-}
