65ef19c344199e3080893954950b25ffaa11c7a8
lyhmyd1211 <lyhwyx@vip.qq.com>
2020-06-22 20:41:28 +0800

    fix

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,14 @@
-const plugins = []
+const plugins = [];
 module.exports = {
-  presets: [['@vue/app', { 'useBuiltIns': 'entry' }], ['@babel/preset-env', {
-    'useBuiltIns': 'usage',
-    'corejs': 3
-  }]],
-  plugins: plugins
-}
+  presets: [
+    ["@vue/app", { useBuiltIns: "entry" }],
+    [
+      "@babel/preset-env",
+      {
+        useBuiltIns: "entry",
+        corejs: 3,
+      },
+    ],
+  ],
+  plugins: plugins,
+};
