617c36c91830cb54504a46b72a4fffd353c76dc1
lyhmyd1211 <lyhwyx@vip.qq.com>
2020-06-22 20:30:35 +0800

    fix

--- src/main.js
+++ src/main.js
@@ -1,22 +1,22 @@
-import Vue from 'vue'
-import BaiduMap from 'vue-baidu-map'
+import Vue from "vue";
+import BaiduMap from "vue-baidu-map";
 
-import 'normalize.css/normalize.css' // A modern alternative to CSS resets
+import "normalize.css/normalize.css"; // A modern alternative to CSS resets
 
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-import locale from 'element-ui/lib/locale/lang/en' // lang i18n
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import locale from "element-ui/lib/locale/lang/en"; // lang i18n
+import "@babel/polyfill";
+import "@/styles/index.scss"; // global css
 
-import '@/styles/index.scss' // global css
+import dataV from "@jiaminghi/data-view";
 
-import dataV from '@jiaminghi/data-view'
+import App from "./App";
+import store from "./store";
+import router from "./router";
 
-import App from './App'
-import store from './store'
-import router from './router'
-
-import '@/icons' // icon
-import '@/permission' // permission control
+import "@/icons"; // icon
+import "@/permission"; // permission control
 
 /**
  * If you don't want to use mock-server
@@ -27,39 +27,37 @@ import '@/permission' // permission control
  * please remove it before going online! ! !
  */
 /**global components  Start*/
-import echarts from '@/components/Echarts'
-Vue.component('echarts', echarts)
+import echarts from "@/components/Echarts";
+Vue.component("echarts", echarts);
 
-import vContainer from '@/components/v-ad-container'
-Vue.component('v-container', vContainer)
+import vContainer from "@/components/v-ad-container";
+Vue.component("v-container", vContainer);
 
-import fullPage from '@/components/fullPage/entry'
-Vue.component('full-page', fullPage)
+import fullPage from "@/components/fullPage/entry";
+Vue.component("full-page", fullPage);
 /**global components  End*/
 
-import moment from 'moment'
-Object.defineProperty(Vue.prototype, '$moment', {
-	value: moment
-})
-
+import moment from "moment";
+Object.defineProperty(Vue.prototype, "$moment", {
+  value: moment,
+});
 
 // set ElementUI lang to EN
-Vue.use(ElementUI)
+Vue.use(ElementUI);
 Vue.use(BaiduMap, {
-	// ak 是在百度地图开发者平台申请的密钥 详见 http://lbsyun.baidu.com/apiconsole/key */
-	ak: process.env.VUE_APP_AK
-})
+  // ak 是在百度地图开发者平台申请的密钥 详见 http://lbsyun.baidu.com/apiconsole/key */
+  ak: process.env.VUE_APP_AK,
+});
 
-
-Vue.use(dataV)
+Vue.use(dataV);
 // 如果想要中文版 element-ui，按如下方式声明
 // Vue.use(ElementUI)
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-	el: '#app',
-	router,
-	store,
-	render: h => h(App)
-})
+  el: "#app",
+  router,
+  store,
+  render: (h) => h(App),
+});
